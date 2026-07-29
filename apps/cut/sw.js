b78cac827bea3107e1933a6a0979049d6f46390f
Yusuf-Gadelrab <yusuf.gadelrab06@gmail.com>
2026-07-29 09:36:36 -0700

    Updates

--- apps/cut/sw.js
+++ apps/cut/sw.js
@@ -1,4 +1,4 @@
-var CACHE = "cut-v5";
+var CACHE = "cut-v6";
 var PRECACHE = ["./", "./index.html", "./manifest.webmanifest"];
 
 self.addEventListener("install", function (e) {
--- apps/fire/sw.js
+++ apps/fire/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'fire-v5';
+const CACHE = 'fire-v6';
 const ASSETS = ['./', './index.html', './manifest.webmanifest'];
 
 self.addEventListener('install', e => {
